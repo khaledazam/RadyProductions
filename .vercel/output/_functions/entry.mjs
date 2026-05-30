383eee80a594d8f3a338c07c81b07e334b31ef9e
khaledazam <khaled141ma@gmail.com>
2026-05-31 00:31:08 +0300

    fix: remove .vercel from git tracking to allow proper vercel builds

--- .vercel/output/_functions/entry.mjs
+++
@@ -1,1 +0,0 @@
-export { l as default } from './chunks/entrypoint_Ba3ZoNC2.mjs';
--- .vercel/output/static/_astro/index.astro_astro_type_script_index_0_lang.Z90ZCwNp.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./index.xgxdCp6f.js";import{S as g}from"./ScrollTrigger.ak1EnQU5.js";r.registerPlugin(g);
